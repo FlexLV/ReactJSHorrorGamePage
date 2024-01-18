9ca69b99f0ceec64ad2706423897d207071bcfbd
Ernests Dejus <ernisd621@gmail.com>
2024-01-18 15:25:27 +0200

    still making donwload page

--- src/Components/Donwloading.jsx
+++ src/Components/Donwloading.jsx
@@ -2,11 +2,24 @@
 import "./Donwloading.css";
 
 function Donwloading(){
+    const handleDownload = () => {
+        // Example: downloading a text file
+        const fileData = "Game not ready yet!";
+        const blob = new Blob([fileData], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+    
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'example.txt';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+      };
     return(
         <div className="donwloading-container">
             <div className="donwloading-wraper">
                 <div className="donwload-part">
-                    <h1 className="message">Start donwloading right now by pressing button down below
                     <span className="drop"></span>
                     <span className="drop"></span>
                     <span className="drop"></span>
@@ -14,8 +27,12 @@ function Donwloading(){
                     <span className="drop"></span>
                     <span className="drop"></span>
                     <span className="drop"></span>
-                    </h1>
-                    <button>Donwload now</button>
+                    <h1 className="message">Begin Your Nightmare</h1>
+                    <button onClick={handleDownload}>DONWLOAD NOW</button>
+                    <div className="watersplash"></div>
+                    <span  className="wave"></span>
+                    <span className="wave"></span>
+
                 </div>
             </div>
         </div>
